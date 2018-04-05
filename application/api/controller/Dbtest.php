f2ae34859718b6674b7eee59d7ab97df3e648d86
724789975 <724789975@qq.com>
2018-04-06 03:49:16 +0800

    redis 配置
    Signed-off-by: 724789975 <724789975@qq.com>

--- application/api/controller/Dbtest.php
+++ application/api/controller/Dbtest.php
@@ -2,11 +2,16 @@
 namespace app\api\controller;
 
 use think\Db;
+use think\cache\driver\Redis;
+
 class Dbtest
 {
     public function index()
     {
-        print_r(Db::query("select * from groups"));
+        $ret = Db::query("select * from groups");
+        $redis_config = include __DIR__."/../redis.php";
+        $redis = new Redis($redis_config);
+        $redis->set('test','hello redis');
         return 'test';
     }
 }
---
+++ application/api/redis.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'host'       => '127.0.0.1',
+    'port'       => 16379,
+    'password'   => '1',
+    'select'     => 0,
+    'timeout'    => 0,
+    'expire'     => 0,
+    'persistent' => true,
+    'prefix'     => '',
+];
+?>
