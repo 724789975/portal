4d8b2c74f1fa9040e66be784a1063026e8123d90
724789975 <724789975@qq.com>
2018-04-10 01:01:55 +0800

    服务器列表
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ application/api/controller/ServerList.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\api\controller;
+
+class ServerList
+{
+    public function index()
+    {
+        $arrServerInfo = array(
+            array(
+                "login_port" => 31002,
+                "login_ip" => "127.0.0.1",
+                "url_host" => "http://127.0.0.1",
+            ),
+            array(
+                "login_port" => 31002,
+                "login_ip" => "quchifan.wang",
+                "url_host" => "http://quchifan.wang",
+            ),
+        );
+        die(json_encode($arrServerInfo));
+    }
+}
